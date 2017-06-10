c412ca7d148bb552ad31fa43284c5e42eaee8920
Casey <casey.zhao@bluepay.asia>
2017-06-10 17:27:35 +0800

    通过javaconfig 装配 bean

---
+++ Ch02/javaconfig/src/test/java/soundsystem2/CDPlayerTest.java
@@ -0,0 +1,36 @@
+package soundsystem2;
+
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import soundsystem2.CDPlayerConfig;
+import soundsystem2.CompactDisc;
+import soundsystem2.MediaPlayer;
+
+/**
+ * Created with Project: SpringiA4
+ * User: Casey
+ * Date: 2017/6/10　15:15
+ * Description:
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= CDPlayerConfig.class) // 告诉测试类在CDPlayerConfig中加载配置
+public class CDPlayerTest {
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+    @Autowired
+    private MediaPlayer player;
+
+
+    @Test
+    public void play() {
+        player.play();
+        assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\r\n",log.getLog());
+    }
+}
